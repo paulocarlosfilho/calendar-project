8b6185be65917cda2858a751970fcf2dae8e0ba9
Paulo Carlos <paulocarlosfilho@gmail.com>
2025-09-23 14:27:45 -0300

    Feature: Criando o connection.php

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+
+// 1. Connection to Local MysQL Server (using XAMPP or MAMPP)
+
+$username = "root"
+$conn - new mysqli("Localhost", $username, "my-calendar");
+$conn>set_charset("utf8mb4");
