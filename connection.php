d7ae8542947eecd0340c12d47422bb2accc5807a
Paulo Carlos <paulocarlosfilho@gmail.com>
2025-10-03 08:17:43 -0300

    Feature: Atualuzando o connection.php - Adicionando o Docker

--- connection.php
+++ connection.php
@@ -2,12 +2,13 @@
 
 // 1. Connection to Local MySQL Server (using XAMPP or MAMPP)
 
+$host = "db"; 
 $username = "root"; 
 $password = "";
 $dbname = "meu_calendario";
 
 // Conexão com 4 parâmetros: (servidor, usuário, senha, nome_do_banco)
-$conn = new mysqli("localhost", $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
